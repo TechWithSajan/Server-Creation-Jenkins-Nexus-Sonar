d783f9836c1ab9f9e0e64587791de9ef539d570b
Sajan Nikam <sajannikam111@gmail.com>
2026-03-14 16:56:43 +0530

    Refactor Jenkins installation script commands
    Updated commands to check Java version and fixed formatting.

--- jenkins-server.sh
+++ jenkins-server.sh
@@ -5,17 +5,20 @@
 # Jenkins Installation
 ################################################################################################
 
-apt update -y  # It will update repo 
-sudo apt install fontconfig openjdk-21-jre # It will install Openjdk
-openjdk --version 
+sudo apt update
+sudo apt install fontconfig openjdk-21-jre
+java -version
 
 
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+  
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
+  
 sudo apt update
+
 sudo apt install jenkins -y
 
 echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
