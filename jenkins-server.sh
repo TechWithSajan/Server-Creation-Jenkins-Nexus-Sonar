fb1e534eace63ea95756fb99f71c782a3c67f27a
Sajan Nikam <sajannikam111@gmail.com>
2026-03-21 08:46:06 +0530

    Update jenkins-server.sh

--- jenkins-server.sh
+++ jenkins-server.sh
@@ -18,7 +18,7 @@ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
 https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
 /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-sudo apt update -y
+sudo apt update
 sudo apt install jenkins -y
 
 sudo systemctl start jenkins
